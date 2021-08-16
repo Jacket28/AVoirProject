6b64185311dd71ebc6cc80cb665b32089812fac0
Sepkohn <valentin.haenggeli@students.hevs.ch>
2021-08-16 20:30:37 +0200

    Update session
    Let session open when user leaves the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:a_voir_app/pages/allEventPage.dart';
 import 'package:a_voir_app/pages/loginPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 //This page is the initialization page which call the first page of the app (allEventPage).
 void main() async {
@@ -16,14 +18,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //FutureBuilder is created for using asynchronous calls since we have a fireBase databse.
-    return FutureBuilder(builder: (context, snapshot) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            key: _scaffoldKey,
-            resizeToAvoidBottomInset: false,
-            body: LoginPage()),
-      );
-    });
+    return FutureBuilder(
+        future: _testRefs(context),
+        builder:
+            (BuildContext context, AsyncSnapshot<SharedPreferences> snapshot) {
+          var page;
+          if (snapshot.data!.getString('userId') != null) {
+            page = AllEventPage();
+          } else {
+            print('no user detected');
+            page = LoginPage();
+          }
+          return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: Scaffold(
+                key: _scaffoldKey,
+                resizeToAvoidBottomInset: false,
+                body: page,
+              ));
+        });
+  }
+
+  Future<SharedPreferences> _testRefs(BuildContext context) async {
+    return SharedPreferences.getInstance();
   }
 }
